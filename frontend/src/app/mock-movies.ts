f5b9da8e9c620e6251fc725f5b9cc8f1f58a38ba
Orsolya <orsolya.suto@ericsson.com>
2020-03-20 17:04:10 +0100

    started with the frontend - basic mock data rendering

---
+++ frontend/src/app/mock-movies.ts
@@ -0,0 +1,39 @@
+import { Movie } from './movie';
+
+export const MOVIES: Movie[] = [
+    {
+        id: 1,
+        original_title: '12 Angry Men',
+        hungraian_title: '12 dühös ember',
+        year: 1957,
+        cities: ['New York']
+    },
+    {
+        id: 2,
+        original_title: 'Atomic Blonde',
+        hungraian_title: 'Atomszőke',
+        year: 2017,
+        cities: ['Berlin', 'Budapest', 'London']
+    },
+    {
+        id: 3,
+        original_title: 'Blade Runner 2049',
+        hungraian_title: 'Blade Runner 2049',
+        year: 2017,
+        cities: ['Etyek', 'New York', 'Sevilla']
+    },
+    {
+        id: 4,
+        original_title: 'Spy',
+        hungraian_title: 'A k;m',
+        year: 2015,
+        cities: ['Budapest', 'Tihany', 'Fót', 'Róma', 'Párizs']
+    },
+    {
+        id: 5,
+        original_title: 'Sunshine',
+        hungraian_title: 'A napfény íze',
+        year: 1999,
+        cities: ['Budapest', 'Vienna', 'Berlin', 'Paris']
+    }
+];
---
+++ frontend/src/app/movie.ts
@@ -0,0 +1,7 @@
+export interface Movie {
+    id: number;
+    original_title: string;
+    hungraian_title: string;
+    year: number;
+    cities: string[];
+}
